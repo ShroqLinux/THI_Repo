eba8ed757988d8491ab4afa5367f5e7853aca675
Shrey InWSL <razehd22@gmail.com>
2024-05-22 18:34:01 +0200

    finished without cond var

--- BS_Prak/Threads/primeMultiNoCond.c
+++ BS_Prak/Threads/primeMultiNoCond.c
@@ -1,148 +1,136 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-int current_number = 2; // Start from the first prime number
-int max_number;
-pthread_mutex_t number_lock;
+int max;
 int numThreads;
-/*
-pthread_mutex_t finish_lock;
-pthread_cond_t finish_cond;
-int finished_threads = 0;
-*/
-int cnt = 0;
-pthread_mutex_t cnt_lock;
+int currentVal = 2;
+int totalPrimes = 0;
 
-typedef struct Range
-{
-    double exec_time;
-} Range;
+pthread_mutex_t value_lck;
+pthread_mutex_t cnt_lck;
 
 bool checkPrime(int a)
 {
-    if (a <= 1)
-    {
-        return false;
-    }
-
-    for (int i = 2; i <= a - 1; i++)
+    for (int i = 2; i < a; i++)
     {
         if (a % i == 0)
         {
             return false;
         }
     }
-
     return true;
 }
 
-void *print_primes(void *arg)
+void *allPrimes(void *args)
 {
-    Range *range = (Range *)arg;
-    struct timespec start, end;
+    double *execTime = (double *)args;
+    // clock_t start = clock();
 
+    struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
 
-    while (1)
+    int thisValue;
+
+    while (currentVal < max)
     {
-        
-        pthread_mutex_lock(&number_lock);
+        pthread_mutex_lock(&value_lck);
+        thisValue = currentVal;
+        currentVal++;
+        pthread_mutex_unlock(&value_lck);
 
-        if (current_number >= max_number)
+        if (checkPrime(thisValue))
         {
-            pthread_mutex_unlock(&number_lock);
-
-            // After finishing the range
-            /*
-            pthread_mutex_lock(&finish_lock);
-            finished_threads++;
-            if (finished_threads == numThreads)
-            {
-                // If this is the last thread to finish, signal the main thread
-                pthread_cond_signal(&finish_cond);
-            }
-            pthread_mutex_unlock(&finish_lock);
-            */
-            clock_gettime(CLOCK_MONOTONIC, &end);
-            range->exec_time += (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-            return NULL;
+            pthread_mutex_lock(&cnt_lck);
+            totalPrimes++;
+            pthread_mutex_unlock(&cnt_lck);
         }
-
-        int number = current_number;
-        current_number++;
-
-        pthread_mutex_unlock(&number_lock);
-
-
-        if (checkPrime(number))
-        {
-            pthread_mutex_lock(&cnt_lock);
-            cnt++;
-            pthread_mutex_unlock(&cnt_lock);
-        }
-
-        
     }
+
+    // clock_t end = clock();
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    //*execTime = (double)(end - start) / CLOCKS_PER_SEC;
+    *execTime = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    // clock_t start = clock();
+
     if (argc != 3)
     {
         printf("Usage: %s (Number of threads) (Max Number)\n", argv[0]);
         return 1;
     }
 
-    numThreads = atoi(argv[1]);
-    max_number = atoi(argv[2]);
+    int numThreads = atoi(argv[1]);
+    max = atoi(argv[2]);
 
+    double execTimes[numThreads];
     pthread_t threads[numThreads];
-    Range ranges[numThreads];
 
-    pthread_mutex_init(&number_lock, NULL);
-    pthread_mutex_init(&cnt_lock, NULL);
-    /*
-    // Initialize the mutex and condition variable
-    pthread_mutex_init(&finish_lock, NULL);
-    pthread_cond_init(&finish_cond, NULL);
-    */
+    int ret;
+
+    ret = pthread_mutex_init(&value_lck, NULL);
+    if (ret != 0)
+    {
+        fprintf(stderr, "Failed to initialize value_lck: %s\n", strerror(ret));
+        return 1;
+    }
+
+    ret = pthread_mutex_init(&cnt_lck, NULL);
+    if (ret != 0)
+    {
+        fprintf(stderr, "Failed to initialize cnt_lck: %s\n", strerror(ret));
+        return 1;
+    }
+
     for (int i = 0; i < numThreads; i++)
     {
-        ranges[i].exec_time = 0.0; // Initialize exec_time to 0
-        pthread_create(&threads[i], NULL, print_primes, &ranges[i]);
+        pthread_create(&threads[i], NULL, allPrimes, &execTimes[i]);
     }
-    /*
-    // Wait for all threads to finish
-    pthread_mutex_lock(&finish_lock);
-    while (finished_threads < numThreads)
-    {
-        pthread_cond_wait(&finish_cond, &finish_lock);
-    }
-    pthread_mutex_unlock(&finish_lock);
-    */
-    pthread_mutex_destroy(&number_lock);
-    pthread_mutex_destroy(&cnt_lock);
 
-    // First loop to join all threads
     for (int i = 0; i < numThreads; i++)
     {
         pthread_join(threads[i], NULL);
     }
-    /*
-     // Destroy the mutex and condition variable
-    pthread_mutex_destroy(&finish_lock);
-    pthread_cond_destroy(&finish_cond);
-    */
-    // Second loop to print the execution times
+
+    ret = pthread_mutex_destroy(&value_lck);
+    if (ret != 0)
+    {
+        fprintf(stderr, "Failed to destroy value_lck: %s\n", strerror(ret));
+        return 1;
+    }
+
+    ret = pthread_mutex_destroy(&cnt_lck);
+    if (ret != 0)
+    {
+        fprintf(stderr, "Failed to destroy cnt_lck: %s\n", strerror(ret));
+        return 1;
+    }
+
     for (int i = 0; i < numThreads; i++)
     {
-        printf("Execution time of thread %d: %f seconds\n", i, ranges[i].exec_time);
+        printf("Execution time thread %d: %f\n", i, execTimes[i]);
     }
-    
-    printf("%d Prime numbers\n", cnt);
+
+    // clock_t end = clock();
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double totalExecTime = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+    // double totalExecTime = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Total prime numbers: %d\n", totalPrimes);
+    printf("Total Execution time: %f\n", totalExecTime);
 
     return 0;
 }
