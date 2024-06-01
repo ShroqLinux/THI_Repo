fdd96e6a3d50aaa91d2483f39e728d4593d5bf66
Shrey WSLMain <razehd22@gmail.com>
2024-06-01 16:09:00 +0200

    finished, need to understand thread execution time calc

--- BS_Prak/Threads/Termin3/producerConsumer.cpp
+++ BS_Prak/Threads/Termin3/producerConsumer.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <semaphore>
-#include <pthread.h>
+
 
 std::vector<int> numList{};
 std::counting_semaphore<1> listLock{1};
 
 std::counting_semaphore<1> cntLock{1};
 int cnt{};
+
+int max{};
 
 bool checkPrime (int toCheck) {
 
@@ -26,56 +28,93 @@ bool checkPrime (int toCheck) {
 }
 
 void addNum() {
-    srand(pthread_self());
-    listLock.acquire();
-    numList.insert(numList.begin(), rand());
-    listLock.release();
+    while (true) {
+
+        cntLock.acquire();
+        int currentCnt = cnt;
+        cntLock.release();
+
+        if (currentCnt >= max) {
+            break;
+        }
+
+        listLock.acquire();
+        numList.insert(numList.begin(), rand());
+        listLock.release();
+    }
 }
 
 void useNum() {
     int thisVal{};
 
-    listLock.acquire();
+    while (true) {
+        listLock.acquire();
 
-    if (!numList.empty()) {
-        thisVal = numList.back();
-        numList.pop_back();
+        if (!numList.empty()) {
+            thisVal = numList.back();
+            numList.pop_back();
+        }
+
+        listLock.release();
+
+        if(checkPrime(thisVal)) {
+
+            cntLock.acquire();
+            if (cnt >= max) {
+                cntLock.release();
+                break;
+            }
+
+            std::cout << thisVal << " is Prime" << std::endl;
+            cnt++;
+            cntLock.release();
+        }
     }
-
-    listLock.release();
-
-    
-    if(checkPrime(thisVal)) {
-        std::cout << thisVal << std::endl;
-        // cntLock.acquire();
-        // cnt++;
-        // cntLock.release();
-    }
-    
 }
 
 int main (int argc, char* argv[]) {
 
-    if(argc != 3) {
-        std::cout << "Usage:" << argv[0] << "(Number producers)" << "(Number consumers)" << std::endl;
+    if(argc != 4) {
+        std::cout << "Usage:" << argv[0] << "(Number producers)" << "(Number consumers)" << "(Max prime numbers)" << std::endl;
         return 1;
     }
 
     int numProducers = atoi(argv[1]);
     int numConsumers = atoi(argv[2]);
+    max = atoi(argv[3]);
 
     std::vector<std::thread> threads{};
+    std::vector<float> execTimes(numProducers);
+
+    std::mutex execTimesMutex;
 
     for (int i = 0; i < numProducers; i++) {
-        threads.emplace_back(addNum);
+        threads.emplace_back([&, i]() {
+            struct timespec start, end;
+            clock_gettime(CLOCK_REALTIME, &start);
+            addNum();
+            clock_gettime(CLOCK_REALTIME, &end);
+            float execTime = (end.tv_sec - start.tv_sec) + 
+                             (end.tv_nsec - start.tv_nsec) / 1000000000.0f;
+            {
+                std::lock_guard<std::mutex> lock(execTimesMutex);
+                execTimes[i] = execTime;
+            }
+        });
     }
+
     for (int i = 0; i < numConsumers; i++) {
         threads.emplace_back(useNum);
     }
+
     for (auto& thread: threads) {
         if (thread.joinable()) {
             thread.join();
         }
+    }
+
+    for(size_t i = 0; i < execTimes.size(); i++) {
+        std::cout << "Thread " << i << ": " << execTimes[i] << std::endl;
     }
     
     return 0;
