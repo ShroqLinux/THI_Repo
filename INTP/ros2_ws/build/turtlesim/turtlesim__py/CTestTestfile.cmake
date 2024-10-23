0e651cd73733d77cbbd03a5911d0211562543c51
Shrey InWSL <razehd22@gmail.com>
2024-10-23 20:57:52 +0200

    added ros2 workspace and build

---
+++ INTP/ros2_ws/build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shrey/workspace/Inf2_MCT_Prak/INTP/ros2_ws/build/turtlesim/turtlesim__py
+# Build directory: /home/shrey/workspace/Inf2_MCT_Prak/INTP/ros2_ws/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
